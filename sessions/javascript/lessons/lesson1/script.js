2e84858f93ab327e225d5d2e8668984781c8a8e5
Pllumbiii <keqmezipllumbb@gmail.com>
2025-05-12 22:45:24 +0200

    .

---
+++ sessions/javascript/lessons/lesson1/script.js
@@ -0,0 +1,22 @@
+// let name = prompt("Shkruaj emrin");
+
+// console.log("Emri:",name);
+// console.log("Emri im eshte" + " " + name);
+// console.log(`Emri im eshte ${name}`);
+
+
+// let age = prompt("Mosha juaj");
+// let year = prompt("Viti qe jeni");
+// let viti = year-age;
+// alert(`viti qe ju keni lindur eshte  ${viti}`);
+
+// let firstNumber = prompt("Numri i pare");
+// let secondNumber = prompt("Numri i dyte");
+
+// alert(`shuma eshte: ${Number(firstNumber)+Number(secondNumber)}`);
+
+let name = prompt("emri");
+let surname = prompt("mbiemri juaj");
+let city = prompt("qyteti ku ju jetoni");
+
+alert(name + " " + surname + " " + city);
